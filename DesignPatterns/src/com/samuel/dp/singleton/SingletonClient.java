a82f5985267c665e05ee3b64cccfc320c1f7246b
TechAlcove <michael.ashley.samuel@gmail.com>
2017-05-05 20:11:34 +0530

    Added call to Singleton method

--- DesignPatterns/src/com/samuel/dp/singleton/SingletonClient.java
+++ DesignPatterns/src/com/samuel/dp/singleton/SingletonClient.java
@@ -5,6 +5,7 @@ public class SingletonClient {
 	public static void main(String[] args) {
 		//Singleton s = new Singleton();
 		Singleton s = Singleton.getInstance();
+		s.myMethod("Santosh");
 
 	}
 
